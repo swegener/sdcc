6eba5f2c3887499a30e9417d1d9ad4fbd7276a26
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2019-04-21 07:21:42 +0000

    Fix bug #2904.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@11237 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- src/pdk/gen.c
+++ src/pdk/gen.c
@@ -1168,7 +1168,7 @@ genSub (const iCode *ic, asmop *result_aop, asmop *left_aop, asmop *right_aop)
           cost (1000, 1000);
         else
           wassertl (0, "Unimplemented p result in subtraction with stack operand");
-      if (i + 1 < size && result_aop->type == AOP_STK && (aopInReg (left_aop, i + 1, P_IDX) || aopInReg (right_aop + 1, i, P_IDX)))
+      if (i + 1 < size && result_aop->type == AOP_STK && (aopInReg (left_aop, i + 1, P_IDX) || aopInReg (right_aop, i + 1, P_IDX)))
         if (regalloc_dry_run)
           cost (1000, 1000);
         else

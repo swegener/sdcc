f55fe17415dc48c0b44ae80bdb27eec83dc4671a
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2019-12-09 13:03:00 +0000

    Improve handling of ADDRESS_OF.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@11484 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- src/SDCClospre.cc
+++ src/SDCClospre.cc
@@ -95,7 +95,8 @@ candidate_expression (const iCode *const ic, int lkey)
     ic->op != RIGHT_OP &&
     !(ic->op == '=' && !POINTER_SET(ic) && !(IS_ITEMP(IC_RIGHT(ic)) /*&& IC_RIGHT(ic)->key > lkey*/)) &&
     ic->op != GET_VALUE_AT_ADDRESS &&
-    ic->op != CAST)
+    ic->op != CAST /*&&
+    ic->op != ADDRESS_OF Apparently typically not worth the cost in code size*/)
     return (false);
 
   const operand *const left = IC_LEFT (ic);
@@ -170,12 +171,14 @@ invalidates_expression(const iCode *const eic, const iCode *const iic)
   const operand *const right = IC_RIGHT (iic);
   const operand *const result = IC_RESULT (iic);
 
+  if (iic->op == FUNCTION || iic->op == ENDFUNCTION || iic->op == RECEIVE)
+    return(true);
+  if (eic->op == ADDRESS_OF) // ADDRESS_OF does not really read its operand.
+    return(false);
   if (eic->op == GET_VALUE_AT_ADDRESS && (isOperandGlobal (IC_RESULT (iic)) || IS_SYMOP (IC_RESULT (iic)) && OP_SYMBOL_CONST (IC_RESULT (iic))->addrtaken))
     return(true);
   if (IC_RESULT (iic) && !IS_OP_LITERAL (result) && !POINTER_SET(iic) &&
     (eleft && isOperandEqual (eleft, result) || eright && isOperandEqual (eright, result)))
-    return(true);
-  if (iic->op == FUNCTION || iic->op == ENDFUNCTION || iic->op == RECEIVE)
     return(true);
   if ((uses_global || uses_volatile) && (iic->op == CALL || iic->op == PCALL))
     return(true);

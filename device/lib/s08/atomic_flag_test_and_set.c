d88327818ddd31a8ae57b6a374951330b47d7bc0
maartenbrock <maartenbrock@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2020-05-29 09:10:59 +0000

    * device/lib/hc08/atomic_flag_test_and_set.c: fix warning * device/lib/s08/atomic_flag_test_and_set.c: fix warning
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@11635 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/lib/s08/atomic_flag_test_and_set.c
+++ device/lib/s08/atomic_flag_test_and_set.c
@@ -13,7 +13,7 @@
 ;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ;  GNU General Public License for more details.
 ;
-;  You should have received a copy of the GNU General Public License 
+;  You should have received a copy of the GNU General Public License
 ;  along with this library; see the file COPYING. If not, write to the
 ;  Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
 ;   MA 02110-1301, USA.
@@ -30,6 +30,7 @@
 
 _Bool atomic_flag_test_and_set(volatile atomic_flag *object) __naked
 {
+  object; // avoid warning 85 unreferenced function argument
 __asm
 	pshx
 	pulh

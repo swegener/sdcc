4a874ab7a881e96ab36302ea1d670df5c1940be8
basxto <basxto@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2020-07-24 00:02:30 +0000

    Fix address translation bug found by toxa
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@11768 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdas/linksrc/lkout.c
+++ sdas/linksrc/lkout.c
@@ -394,16 +394,13 @@ iflush()
         a_uint chksum, lo_addr, hi_addr;
         // rom addresses, calculated based on the virtual ones
         a_uint rrtadr0 = rtadr0;
-        a_uint rrtadr1 = rtadr1;
         // translate virtual addresses for gameboy
         if(TARGET_IS_GB){
                 if(rrtadr0 > 0x10000)
                         rrtadr0 = (rrtadr0>>16) * 0x4000 + (rrtadr0&0xffff) - 0x4000;
-                if(rrtadr1 > 0x10000)
-                        rrtadr1 = (rrtadr1>>16) * 0x4000 + (rrtadr1&0xffff) - 0x4000;
         }
 
-        max = (int) (rrtadr1 - rrtadr0);
+        max = (int) (rtadr1 - rtadr0);
         if (max) {
                 if (a_bytes > 2) {
                         static a_uint prev_hi_addr = 0;

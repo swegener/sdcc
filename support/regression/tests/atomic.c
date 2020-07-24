97ec85e8c30c3323309be9f0a752029eb131044a
maartenbrock <maartenbrock@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2020-07-24 15:59:26 +0000

    * device/include/stdatomic.h: enable mcs51, atomic_flag must reside in data/idata * device/lib/mcs51/Makefile.in: added atomic_flag_test_and_set.rel & atomic_flag_clear.rel,   fixed include path * support/regression/tests/atomic.c: enabled test for mcs51,   added test for atomic_flag within struct
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@11771 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/atomic.c
+++ support/regression/tests/atomic.c
@@ -3,25 +3,37 @@
 #include <testfwk.h>
 
 // Some ports do not have atomic_flag yet.
-#if !defined(__SDCC_mcs51) && !defined(__SDCC_ds390) && !defined(__SDCC_pic14) && !defined(__SDCC_pic16) && !defined(__SDCC_pdk13) && !defined(__SDCC_pdk14) && !defined(__SDCC_pdk15) && !defined(__SDCC_tlcs90)
+#if !defined(__SDCC_ds390) && !defined(__SDCC_pic14) && !defined(__SDCC_pic16) && !defined(__SDCC_pdk13) && !defined(__SDCC_pdk14) && !defined(__SDCC_pdk15) && !defined(__SDCC_tlcs90)
 
 #include <stdatomic.h>
 #include <stdbool.h>
 
-atomic_flag f1 = ATOMIC_FLAG_INIT;
-atomic_flag f2;
+#if defined(__SDCC_mcs51) && !defined(__SDCC_MODEL_SMALL)
+#define memory __idata	/* mcs51 atomic_flag must be in __data or __idata */
+#else
+#define memory
+#endif
+
+memory atomic_flag f1 = ATOMIC_FLAG_INIT;
+memory atomic_flag f2;
+memory struct { int a; atomic_flag f; } s = {0, ATOMIC_FLAG_INIT};
 
 #endif
 
 void testAtomic(void)
 {
-#if !defined(__SDCC_mcs51) && !defined(__SDCC_ds390) && !defined(__SDCC_pic14) && !defined(__SDCC_pic16) && !defined(__SDCC_pdk13) && !defined(__SDCC_pdk14) && !defined(__SDCC_pdk15) && !defined(__SDCC_tlcs90)
+#if !defined(__SDCC_ds390) && !defined(__SDCC_pic14) && !defined(__SDCC_pic16) && !defined(__SDCC_pdk13) && !defined(__SDCC_pdk14) && !defined(__SDCC_pdk15) && !defined(__SDCC_tlcs90)
 	ASSERT(atomic_flag_test_and_set(&f1) == false);
 	ASSERT(atomic_flag_test_and_set(&f1) == true);
 	atomic_flag_clear(&f1);
 	atomic_flag_clear(&f2);
 	ASSERT(atomic_flag_test_and_set(&f1) == false);
 	ASSERT(atomic_flag_test_and_set(&f2) == false);
+
+	ASSERT(atomic_flag_test_and_set(&s.f) == false);
+	ASSERT(atomic_flag_test_and_set(&s.f) == true);
+	atomic_flag_clear(&s.f);
+	ASSERT(atomic_flag_test_and_set(&s.f) == false);
 #endif
 }
 

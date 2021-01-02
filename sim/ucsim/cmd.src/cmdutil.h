66bea7311d58ad78f91c898f168bb3e9df9d37e0
epetrich <epetrich@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2021-01-02 20:38:26 +0000

    * sim/ucsim/cmd.src/cmdlex.l: reverted previous change * sim/ucism/cmd.src/cmdutil.h: ensure SOCKET_AVAIL defined before use
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@12009 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sim/ucsim/cmd.src/cmdutil.h
+++ sim/ucsim/cmd.src/cmdutil.h
@@ -28,11 +28,11 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #ifndef CMD_CMDUTIL_HEADER
 #define CMD_CMDUTIL_HEADER
 
+#include "ddconfig.h"
+
 #ifdef SOCKET_AVAIL
 # include HEADER_SOCKET
 #endif
-
-#include "ddconfig.h"
 
 
 extern char *proc_escape(char *string, int *len);

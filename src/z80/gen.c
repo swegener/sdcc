f352a6afd56763b8e52c9559b1215270107815c9
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2021-03-25 17:37:44 +0000

    Fix an issue affecting 4-byte swap with both operands on extended stack.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@12170 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- src/z80/gen.c
+++ src/z80/gen.c
@@ -3143,7 +3143,7 @@ cheapMove (asmop *to, int to_offset, asmop *from, int from_offset, bool a_dead)
     from->type == AOP_SFR ||
     to->type == AOP_SFR ||
     to->type == AOP_STK && from->type == AOP_STK ||
-    (to->type == AOP_IY || to->type == AOP_HL) && (from->type == AOP_EXSTK || IS_GB && from->type == AOP_STK) ||
+    (to->type == AOP_HL || to->type == AOP_IY || to->type == AOP_EXSTK) && (from->type == AOP_HL || from->type == AOP_IY || from->type == AOP_EXSTK || IS_GB && from->type == AOP_STK) ||
     (to->type == AOP_HL || IS_GB && to->type == AOP_STK || to->type == AOP_EXSTK) && (aopInReg(from, from_offset, L_IDX) || aopInReg(from, from_offset, H_IDX))))
     {
       if (!a_dead)

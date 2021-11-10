dde58ae203b4b1988da715bda0a1c0e51c0b7ecf
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2021-11-10 14:02:41 +0000

    Fix handling of atomic_flag with clear state 1.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@12784 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/lib/atomic_flag_clear.c
+++ device/lib/atomic_flag_clear.c
@@ -34,6 +34,7 @@ void atomic_flag_clear(volatile atomic_flag *object)
 	object->flag = 0;
 #elif defined(__SDCC_z80) || defined(__SDCC_z180) || defined(__SDCC_ez80_z80) || defined(__SDCC_z80n) || defined(__SDCC_gbz80) || defined(__SDCC_r2k) || defined(__SDCC_r2ka) || defined(__SDCC_r3ka) || defined(__SDCC_stm8) || defined(__SDCC_hc08) || defined(__SDCC_s08)
 	object->flag = 1;
+#else
 #error Support for atomic_flag not implemented
 #endif
 }

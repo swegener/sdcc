a622680f04210679b9d7babf99c869f71373cb6e
gorlik <gorlik@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2022-01-06 16:51:39 +0000

    Rename m6502 port to mos6502.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@12839 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/ports/uc6502/spec.mk
+++ support/regression/ports/uc6502/spec.mk
@@ -1,11 +1,9 @@
-# Regression test specification for the m6502 target running with sim65
+# Regression test specification for the mos6502 target running with uCsim
 
 # simulation timeout in cycles
 SIM_CYCLES = 1000000000
 SIM_TIMEOUT = 30
 
-# path to sim65
-#EMU = $(WINE) sim65 -c -v -x $(SIM_CYCLES)
 ifdef SDCC_BIN_PATH
   AS = $(SDCC_BIN_PATH)/sdas6500$(EXEEXT)
   UC65 = $(SDCC_BIN_PATH)/ucsim_mcs6502$(EXEEXT)
@@ -18,7 +16,7 @@ else
   AS = $(WINE) $(top_builddir)/bin/sdas6500$(EXEEXT)
 ifndef CROSSCOMPILING
   SDCCFLAGS += --nostdinc -I$(top_srcdir)
-  LINKFLAGS += --nostdlib -L$(top_builddir)/device/lib/build/m6502
+  LINKFLAGS += --nostdlib -L$(top_builddir)/device/lib/build/mos6502
 endif
 endif
 
@@ -28,8 +26,8 @@ ifdef CROSSCOMPILING
   SDCCFLAGS += -I$(top_srcdir)
 endif
 
-SDCCFLAGS += -mm6502 --less-pedantic --out-fmt-ihx --code-loc 0x200 --xram-loc 0xc000
-LINKFLAGS += m6502.lib
+SDCCFLAGS += -mmos6502 --less-pedantic --out-fmt-ihx --code-loc 0x200 --xram-loc 0xc000
+LINKFLAGS += mos6502.lib
 
 OBJEXT = .rel
 BINEXT = .ihx
@@ -59,17 +57,6 @@ $(PORT_CASES_DIR)/%$(OBJEXT): $(srcdir)/fwk/lib/%.c
 
 $(PORT_CASES_DIR)/fwk.lib: $(srcdir)/fwk/lib/fwk.lib
 	cat < $(srcdir)/fwk/lib/fwk.lib > $@
-
-# run simulator with SIM_TIMEOUT seconds timeout
-#%.out: %$(BINEXT) $(CASES_DIR)/timeout
-#	mkdir -p $(dir $@)
-#	makebin -s 65536 $< $*.rom
-#	printf 'sim65\2\0\0\0\2\0\2' > $*.bin
-#	dd status=none if=$*.rom bs=512 count=126 skip=1 >> $*.bin
-#	-$(CASES_DIR)/timeout $(SIM_TIMEOUT) $(EMU) $*.bin > $@ \
-#	  || echo -e --- FAIL: \"timeout, simulation killed\" in $(<:$(BINEXT)=.c)"\n"--- Summary: 1/1/1: timeout >> $@
-#	$(PYTHON) $(srcdir)/get_ticks.py < $@ >> $@
-#	-grep -n FAIL $@ /dev/null || true
 
 # run simulator with SIM_TIMEOUT seconds timeout
 %.out: %$(BINEXT) $(CASES_DIR)/timeout

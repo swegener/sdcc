f40d8410bce539f47a7acf97f4883ff8c71806ec
gorlik <gorlik@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2022-01-13 18:53:49 +0000

    Revert [12873] changes to gcc-torture-execute-981001-1.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@12878 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/gcc-torture-execute-981001-1.c
+++ support/regression/tests/gcc-torture-execute-981001-1.c
@@ -17,29 +17,17 @@ long sub (int n) __reentrant
 {
   int a, b ;
 
-  __prints(" FAIL: n:");
-  __printd(n);
-  __prints("\n");
-  
   if (n >= 2)
     {
       if (n % 2 == 0)
 	{
-	  __prints(" FAIL: even\n");
 	  a = sub (n / 2);
 	  
 	  return (a + 2 * sub (n / 2 - 1)) * a;
 	}
       else
 	{
-	  int tmp;
-	  //  __prints(" FAIL: odd\n");
-	  tmp=n/2+1;
-	  //	  __prints(" FAIL: tmp=");
-	  //	  __printd(tmp);
-	  //	  __prints("\n");
-	  //a = sub (n / 2 + 1);
-	  a = sub (tmp);
+	  a = sub (n / 2 + 1);
 	  b = sub (n / 2);
 	  
 	  return a * a + b * b;
@@ -54,15 +42,8 @@ void
 testTortureExecute (void)
 {
 #if !defined(__SDCC_pdk14) && !defined(__SDCC_pdk15) // Lack of memory
-  long t;
-  t=sub(30);
-
-  __prints(" FAIL: t:");
-  __printd((int)t);
-  __prints("\n");
-  
-    if (sub (30) != 832040L)
-      flg |= NG;
+  if (sub (30) != 832040L)
+    flg |= NG;
 
   if (flg)
     ASSERT (0);

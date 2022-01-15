1d27805f9e9d055f80b297d0a36cb21e028d017e
gorlik <gorlik@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2022-01-15 07:55:42 +0000

    Disable failing test for DS390 port.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@12886 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/reentrant.c
+++ support/regression/tests/reentrant.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 
 #if !defined( __SDCC_pdk14) && !defined( __SDCC_pdk15) // Lack of memory
+#if !defined( __SDCC_ds390) // Bug 3307
 
 {type} sum({type} a) __reentrant
 {
@@ -33,15 +34,18 @@
 }
 
 #endif
+#endif
 
 void
 testReent(void)
 {
 #if !defined( __SDCC_pdk14) && !defined( __SDCC_pdk15) // Lack of memory
+#if !defined( __SDCC_ds390) // Bug 3307
   ASSERT(sum(9) == 45);
   ASSERT(fact(5) == 120);
   ASSERT(div2n(128, 7) == 1);
   ASSERT(fib(7) == 13);
+#endif
 #endif
 }
 

da97c5ad85e6a83b879aa9bf1af63f75aa64aa44
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2022-02-01 18:43:34 +0000

    Increase some timeouts in hope of making the bug-3129 regression test pass on that slow PPC Mac in the compile farm.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@12953 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/ports/s08/spec.mk
+++ support/regression/ports/s08/spec.mk
@@ -1,7 +1,7 @@
 # Regression test specification for the s08 target running with uCsim
 
 # simulation timeout in seconds
-SIM_TIMEOUT = 40
+SIM_TIMEOUT = 60
 
 EMU_PORT_FLAG=-thcs08
 
--- support/regression/ports/ucgbz80/spec.mk
+++ support/regression/ports/ucgbz80/spec.mk
@@ -1,7 +1,7 @@
 # Regression test specification for the sm83 target running with uCsim
 
 # simulation timeout in seconds
-SIM_TIMEOUT = 40
+SIM_TIMEOUT = 60
 
 EMU_PORT_FLAG=-tlr35902
 

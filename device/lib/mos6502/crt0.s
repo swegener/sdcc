2d07f34cb83b18c4c452d3b765d1da798191802d
gorlik <gorlik@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2022-03-09 23:52:47 +0000

    Use ZP for memcpy and memset parameters.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@13176 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/lib/mos6502/crt0.s
+++ device/lib/mos6502/crt0.s
@@ -36,12 +36,12 @@
         .area RODATA
         .area XINIT
 
-        .area ZP      (PAG)
-        .area OSEG    (PAG, OVR)
-
 	.area _DATA
         .area DATA
         .area BSS
+
+        .area ZP      (PAG)
+        .area OSEG    (PAG, OVR)
 
 	;; Reset/interrupt vectors
         .area   CODEIVT (ABS)
@@ -60,24 +60,24 @@ __sdcc_gs_init_startup:
 __sdcc_init_data:
 ;; initialize DATA
         lda #<s_XINIT
-        sta ___memcpy_PARM_2
+        sta *___memcpy_PARM_2
         lda #>s_XINIT
-        sta ___memcpy_PARM_2+1
+        sta *___memcpy_PARM_2+1
         lda #<l_XINIT
-        sta ___memcpy_PARM_3
+        sta *___memcpy_PARM_3
         lda #>l_XINIT
-        sta ___memcpy_PARM_3+1
+        sta *___memcpy_PARM_3+1
         lda #<s_DATA
         ldx #>s_DATA
         jsr ___memcpy
 
 ;; clear BSS
         lda #0x00
-        sta _memset_PARM_2
+        sta *_memset_PARM_2
         lda #<l_BSS
-        sta _memset_PARM_3
+        sta *_memset_PARM_3
         lda #>l_BSS
-        sta _memset_PARM_3+1
+        sta *_memset_PARM_3+1
         lda #<s_BSS
         ldx #>s_BSS
         jsr _memset

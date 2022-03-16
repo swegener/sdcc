10d01dce0aa57e148c195587d8085246f5e89eed
gorlik <gorlik@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2022-03-16 05:23:56 +0000

    Enable div and ldiv for mos6502 and hc08/s08 ports.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@13244 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/div.c
+++ support/regression/tests/div.c
@@ -8,16 +8,15 @@
 void testDiv(void)
 {
 #ifndef __SDCC_mcs51 // mcs51 does not yet support returning struct
-#if !defined (__SDCC_hc08) && !defined (__SDCC_s08) // segfault
 	ASSERT (div(4223, 23).quot == 4223 / 23);
 	ASSERT (div(4223, 23).rem == 4223 % 23);
 #if !defined(__SDCC_pdk13) && !defined(__SDCC_pdk14) && !defined(__SDCC_pdk15) // Lack of memory
 	ASSERT (ldiv(4223, 23).quot == 4223l / 23);
 	ASSERT (ldiv(4223, 23).rem == 4223l % 23);
-#ifndef __SDCC_ds390 // segfault on compiling lldiv
+#if !defined(__SDCC_ds390) && !defined (__SDCC_hc08) && !defined (__SDCC_s08) && !defined(__SDCC_mos6502) && !defined (__SDCC_mos65c02)
+	// segfault on compiling lldiv
 	ASSERT (lldiv(4223, 23).quot == 4223ll / 23);
 	ASSERT (lldiv(4223, 23).rem == 4223ll % 23);
-#endif
 #endif
 #endif
 #endif

a821b24d65cf82c1d457d8a1b8c3b1451a0124bf
drdani <drdani@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2022-04-19 11:48:03 +0000

    [regression-stm8] Enlarge simulator stack needed by some stm8 tests
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@13389 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/ports/stm8/uCsim.cmd
+++ support/regression/ports/stm8/uCsim.cmd
@@ -3,26 +3,27 @@ set error unknown_code off
 set error memory off
 set error stack off
 set hw simif rom 0x7fff
-uart1_on=0
-uart3_on=0
-dreg0_on=0
-dport0_on=0
-flash0_on=0
-itc0_on=0
-pa0_on=0
-pb0_on=0
-pc0_on=0
-pd0_on=0
-pe0_on=0
-pf0_on=0
-pg0_on=0
-ph0_on=0
-pi0_on=0
-tim11_on=0
-tim22_on=0
-tim33_on=0
-tim44_on=0
-vcd0_on=0
+expr /n sp_limit=0x1000
+expr /n uart1_on=0
+expr /n uart3_on=0
+expr /n dreg0_on=0
+expr /n dport0_on=0
+expr /n flash0_on=0
+expr /n itc0_on=0
+expr /n pa0_on=0
+expr /n pb0_on=0
+expr /n pc0_on=0
+expr /n pd0_on=0
+expr /n pe0_on=0
+expr /n pf0_on=0
+expr /n pg0_on=0
+expr /n ph0_on=0
+expr /n pi0_on=0
+expr /n tim11_on=0
+expr /n tim22_on=0
+expr /n tim33_on=0
+expr /n tim44_on=0
+expr /n vcd0_on=0
 run
 state
 quit

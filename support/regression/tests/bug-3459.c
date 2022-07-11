e915974dcc0ac703b95860355b9d6e33c3ffffc2
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2022-07-11 16:25:50 +0000

    An attempt at creating a regression test for bug #3459.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@13568 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ support/regression/tests/bug-3459.c
@@ -0,0 +1,33 @@
+/* bug-3459.c
+   ?
+ */
+
+#include <testfwk.h>
+
+/// Test derived from code by SF user "Under4Mhz" in 2022.
+/// GPL 2.0 or greater
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#define PICTURE_TILE_COUNT 4
+#define PICTURE1_TILES 34
+#define PICTURE2_TILES ( PICTURE1_TILES + PICTURE_TILE_COUNT )
+
+///< Is matching tiles
+bool IsSameTile( int8_t tile1, int8_t tile2 ) {
+
+    if ( tile1 <= 0 || tile2 <= 0 ) return false;
+
+    bool same = tile1 == tile2;
+    bool flowers = tile1 > PICTURE1_TILES && tile1 <= PICTURE2_TILES && tile2 > PICTURE1_TILES && tile2 <= PICTURE2_TILES;
+    bool seasons = tile1 > PICTURE2_TILES && tile2 > PICTURE2_TILES;
+
+    return same || seasons || flowers;
+}
+
+void testBug() {
+    ASSERT ( IsSameTile(35,36) );
+    ASSERT ( IsSameTile(40,41) );
+}
+

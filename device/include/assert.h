9ad6980830bdc96ddc9603a9d1017f7c5d393d96
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2022-09-29 21:54:51 +0000

    Fix static_assert for C11.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@13726 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/include/assert.h
+++ device/include/assert.h
@@ -42,7 +42,7 @@
 void __assert(const char *expression, const char *functionname, const char *filename, unsigned int linenumber);
 #define assert(...) ((__VA_ARGS__) ? (void)0 : __assert(#__VA_ARGS__, __func__, __FILE__, __LINE__))
 
-#if __STDC_VERSION__ >= 202311L
+#if __STDC_VERSION__ < 202311L
 #define static_assert _Static_assert
 #endif
 

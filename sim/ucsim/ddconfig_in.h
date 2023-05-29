56920a9b13994ff50842cd41efabedcf7a890490
drdani <drdani@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2023-05-29 20:53:38 +0000

    [ucsim] Fix [bugs:#3594] Insufficient curses check in uCsim configure script
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@14091 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sim/ucsim/ddconfig_in.h
+++ sim/ucsim/ddconfig_in.h
@@ -29,6 +29,10 @@
 
 /* XXX */
 #undef HAVE_CURSES_H
+#undef HAVE_NCURSESW_H
+#undef HAVE_NCURSES_H
+#undef HAVE_NCURSESW_CURSES_H
+#undef HAVE_NCURSES_CURSES_H
 
 /* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
    */

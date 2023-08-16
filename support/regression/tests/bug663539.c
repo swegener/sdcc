b4438c7f7359050008b29264d38b2fcd2fb3a7a7
drdani <drdani@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2023-08-16 09:27:28 +0000

    [regression-mcs51] Prepare test for emulation mode
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@14297 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/bug663539.c
+++ support/regression/tests/bug663539.c
@@ -16,6 +16,7 @@ test_volatile (void)
   __prints("--- Summary: 0/1/1: 0 failed of 1 tests in 1 cases.");
 
   x;         //this should end the simulation
+  x= 's';    // emulation mode: stop be simif
 
   while (1); //let the "watchdog" bite
 #endif

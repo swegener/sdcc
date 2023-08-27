c798267405afc718bfd0597002f72dd82828abea
maartenbrock <maartenbrock@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2023-08-27 07:08:10 +0000

    * support/regression/tests/bug-3165.c: enabled for mcs51 & ds390,   fixed warnings
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@14322 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/bug-3165.c
+++ support/regression/tests/bug-3165.c
@@ -5,10 +5,19 @@
 
 #include <testfwk.h>
 
-#if !defined(PORT_HOST) && !defined(__SDCC_stm8) && !defined(__SDCC_hc08) && !defined(__SDCC_s08) && !defined(__SDCC_mos6502) && !defined(__SDCC_mos65c02)&& !defined(__SDCC_mcs51) && !defined(__SDCC_ds390) && !defined(__SDCC_tlcs90)
+#if !defined(PORT_HOST) && !defined(__SDCC_stm8) && !defined(__SDCC_hc08) && !defined(__SDCC_s08) && !defined(__SDCC_mos6502) && !defined(__SDCC_mos65c02) && !defined(__SDCC_tlcs90)
+
 unsigned char effect_y_line;
+#if defined(__SDCC_sm83)
+volatile __sfr __at(0xff02) reg_SCY;
+volatile __sfr __at(0xff04) reg_LY;
+#elif defined(__SDCC_mcs51) || defined(__SDCC_ds390)
+volatile __sfr __at(0x82) reg_SCY;
+volatile __sfr __at(0x84) reg_LY;
+#else
 volatile __sfr __at(0x02) reg_SCY;
 volatile __sfr __at(0x04) reg_LY;
+#endif
 
 void f(void) {
     effect_y_line = 10;

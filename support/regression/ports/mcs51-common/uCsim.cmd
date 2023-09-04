1aa84c2db18bf4ac9d6cfc45bb1d221a4f85b4bd
drdani <drdani@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2023-09-04 08:39:00 +0000

    [regression] Do not use selfjump_stop option
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@14341 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/ports/mcs51-common/uCsim.cmd
+++ support/regression/ports/mcs51-common/uCsim.cmd
@@ -2,7 +2,7 @@ set error non-classified off
 set error unknown_code off
 set error memory off
 set error stack off
-set opt selfjump_stop 1
+set opt selfjump_stop 0
 break xram r 0x7654
 set hw simif xram 0x7654
 expr /n port0_on=0

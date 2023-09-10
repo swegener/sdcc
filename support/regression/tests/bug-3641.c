81fd54241446cbd632ca920b2f75bb4174dbaacb
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2023-09-10 17:24:06 +0000

    Support pre-C23 hosts in test for bug #3641.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@14344 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/bug-3641.c
+++ support/regression/tests/bug-3641.c
@@ -4,19 +4,23 @@
 
 #include <testfwk.h>
 
+#include <stdint.h>
+
+#if __SDCC_BITINT_MAXWIDTH >= 32 // TODO: When we can regression-test in --std-c23 mode, use the standard macro from limits.h instead!
 // In both assignments to i, the least-significant byte gets omitted.
 void loopm2(unsigned char *a)
 {
     for (unsigned _BitInt(24) i = (1ul << 20); i < (1ul << 20) + 1; i = (1ul << 20) + 1)
         a[i - (1ul << 20)] = 1;
 }
+#endif
 
 void
 testBug(void)
 {
+#if __SDCC_BITINT_MAXWIDTH >= 32 // TODO: When we can regression-test in --std-c23 mode, use the standard macro from limits.h instead!
 	char c = 0;
 	loopm2(&c);
-#ifndef __SDCC_pdk15 // Another bug.
 	ASSERT(c == 1);
 #endif
 }

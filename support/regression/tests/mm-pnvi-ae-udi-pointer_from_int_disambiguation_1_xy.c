a5151380bfe9106984c36972281ce96ef38d306a
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2023-09-25 19:37:50 +0000

    Disable test for FreeBSD 13 / clang-14, where it fails due to known bug.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@14359 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/mm-pnvi-ae-udi-pointer_from_int_disambiguation_1_xy.c
+++ support/regression/tests/mm-pnvi-ae-udi-pointer_from_int_disambiguation_1_xy.c
@@ -33,7 +33,7 @@ int x=1, y=2;
 void
 testMM(void)
 {
-#if !defined(__FreeBSD__) || !(defined(__clang__) && __clang_major__ <= 13) // On FreeBSD 13 on arch64, this test fails for clang 11, clang 13 and clang14. On amd64 it fails for clang 6, but passes with clang 14.  FreeBSD bug #273773.
+#if !defined(__FreeBSD__) || !(defined(__clang__) && __clang_major__ <= 14) // On FreeBSD 13 on arch64, this test fails for clang 11, clang 13 and clang14. On amd64 it fails for clang 6, but passes with clang 14.  FreeBSD bug #273773.
   int *p = &x+1;
   int *q = &y;
   uintptr_t i = (uintptr_t)p;

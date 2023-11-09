604506ec665f59ba28a8a44e8abf7c7c5503e9d3
drdani <drdani@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2023-11-09 20:05:34 +0000

    [regression-mcs51] Turn off not needed peripherals, run in emulation mode
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@14414 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/ports/mcs51-common/uCsim.cmd
+++ support/regression/ports/mcs51-common/uCsim.cmd
@@ -9,6 +9,13 @@ expr /n port0_on=0
 expr /n port1_on=0
 expr /n port2_on=0
 expr /n port3_on=0
-run
+expr /n timer00_on=0
+expr /n timer11_on=0
+expr /n timer22_on=0
+expr /n uart0_on=0
+expr /n dreg0_on=0
+expr /n dport0_on=0
+expr /n irq0_on=0
+emu
 state
 quit

e35c6392dfb6fc8147830b5d6b458fbc4eb43785
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2023-12-02 07:55:29 +0000

    Enable struct parameter tests for all ports except mos6502 (which doesn't support them yet).
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@14484 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/bug-2776.c
+++ support/regression/tests/bug-2776.c
@@ -11,7 +11,7 @@
 
 #include <stdarg.h>
 
-#if !defined(__SDCC_mos6502) && !defined(__SDCC_hc08) && !defined(__SDCC_s08) // Todo: enable when struct parameters are supported!
+#if !defined(__SDCC_mos6502) // Todo: enable when struct parameters are supported!
 struct tiny
 {
   char c;
@@ -52,7 +52,7 @@ void f (int n, ...)
 void
 testBug (void)
 {
-#if !defined(__SDCC_mos6502) && !defined(__SDCC_hc08) && !defined(__SDCC_s08) // Todo: enable when struct parameters are supported!
+#if !defined(__SDCC_mos6502) // Todo: enable when struct parameters are supported!
 #ifndef __OpenBSD__ // Known to fail on OpenBSD 7.3 powerpc64 (don't know for other OpenBSD versions or archs) reported to OpenBSD.
   m ();
 #endif

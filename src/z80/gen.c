10a0e230336a89f7da51d7856d97df9ad44ea9ee
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2024-04-19 07:24:08 +0000

    Fix generation of a redundant instruction in z80 comparison.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@14809 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- src/z80/gen.c
+++ src/z80/gen.c
@@ -9740,8 +9740,8 @@ genCmp (operand * left, operand * right, operand * result, iCode * ifx, int sign
         }
       else if (ifx && size == 1 && !sign && aopInReg (right->aop, 0, A_IDX) && left->aop->type == AOP_LIT && ullFromVal (left->aop->aopu.aop_lit) < 255)
         {
-          emit3 (A_CP, ASMOP_A, left->aop);
           emit2 ("cp a, !immedbyte", ullFromVal (left->aop->aopu.aop_lit) + 1);
+          cost2 (2, 7, 6, 4, 8, 6, 2, 2);
           result_in_carry = true;
           inv = true;
           goto release;

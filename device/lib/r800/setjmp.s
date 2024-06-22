bce7a699e55c2db1ce3078314ae7e63db98e9703
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2024-06-22 17:41:41 +0000

    Improve setjmp/longjmp for z80-related ports, patch #479 by Janko Stamenović.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@14892 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/lib/r800/setjmp.s
+++ device/lib/r800/setjmp.s
@@ -1,7 +1,8 @@
 ;--------------------------------------------------------------------------
 ;  setjmp.s
 ;
-;  Copyright (C) 2011-2021, Philipp Klaus Krause
+;  Copyright (C) 2011-2024, Philipp Klaus Krause
+;  Copyright (C) 2024 Janko Stamenović
 ;
 ;  This library is free software; you can redistribute it and/or modify it
 ;  under the terms of the GNU General Public License as published by the
@@ -13,7 +14,7 @@
 ;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ;  GNU General Public License for more details.
 ;
-;  You should have received a copy of the GNU General Public License 
+;  You should have received a copy of the GNU General Public License
 ;  along with this library; see the file COPYING. If not, write to the
 ;  Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
 ;   MA 02110-1301, USA.
@@ -26,67 +27,85 @@
 ;   might be covered by the GNU General Public License.
 ;--------------------------------------------------------------------------
 
-	.area   _CODE
+	.area	_CODE
 
 	.globl ___setjmp
 
 ___setjmp:
-	push	hl
-	pop	iy
+	; store ret addr
+	pop	de
+	push	de
+	ld	(hl), e
+	inc	hl
+	ld	(hl), d
+	inc	hl
 
-	pop	hl
-	push	hl
-
-	; Store return address.
-	ld	0(iy), l
-	ld	1(iy), h
-
-	; Store stack pointer.
+	; store SP
 	xor	a, a
-	ld	l, a
-	ld	h, a
+	ld	e, a
+	ld	d, a
+	ex	de, hl
 	add	hl, sp
-	ld	2(iy), l
-	ld	3(iy), h
+	ex	de, hl
+	ld	(hl), e
+	inc	hl
+	ld	(hl), d
+	inc	hl
 
-	; Store frame pointer.
+	; store frame ptr
 	push	ix
-	pop	hl
-	ld	4(iy), l
-	ld	5(iy), h
+	pop	de
+	ld	(hl), e
+	inc	hl
+	ld	(hl), d
 
-	; Return 0.
+	; ret 0
 	ld	e, a
 	ld	d, a
 	ret
 
+
 .globl _longjmp
 
 _longjmp:
-	push	hl
-	pop	iy
-
-	; Ensure that return value is non-zero.
+	; pass retval as is, only if 0 pass 1
 	ld	a, e
 	or	a, d
-	jr	NZ, jump
-	inc	de
-jump:
+	jr	nz, s1
+	inc	e
+s1:
+	; save retval
+	push	de
 
-	; Restore frame pointer.
-	ld	l, 4(iy)
-	ld	h, 5(iy)
-	push	hl
+	; fetch stored jumpaddr
+	ld	c, (hl)
+	inc	hl
+	ld	b, (hl)
+	inc	hl
+
+	; fetch  spval
+	ld	e, (hl)
+	inc	hl
+	ld	d, (hl)
+	inc	hl
+	; save	spval
+	push	de
+
+	; fetch and set IX
+	ld	e, (hl)
+	inc	hl
+	ld	d, (hl)
+	push	de
 	pop	ix
 
-	; Adjust stack pointer.
-	ld	l, 2(iy)
-	ld	h, 3(iy)
+	; restore spval
+	pop	hl
+	; restore retval
+	pop	de
+	; adjust the stack
 	ld	sp, hl
 	pop	hl
 
-	; Jump.
-	ld	l, 0(iy)
-	ld	h, 1(iy)
-	jp	(hl)
-
+	ld	l, c
+	ld	h, b
+	jp	(hl)  ; jump to jumpaddr

b69fb5a224644b67f2f495be587f51168cda63c3
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2024-12-19 17:17:13 +0000

    Fix comment in Rabbit 3000A startup code.
    git-svn-id: http://svn.code.sourceforge.net/p/sdcc/code/trunk/sdcc@15184 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- device/lib/r3ka/crt0.s
+++ device/lib/r3ka/crt0.s
@@ -75,7 +75,7 @@ MB3CR		.equ	0x17 ; Memory Bank 3 Control Register
 	call ___sdcc_external_startup
 
 	; Initialise global variables. Skip if __sdcc_external_startup returned
-	; non-zero value. Note: calling convention version 0 only.
+	; non-zero value. Note: calling convention version 1 only.
 	or	a, a
 	jr	NZ, skip_gsinit
 	call	gsinit
